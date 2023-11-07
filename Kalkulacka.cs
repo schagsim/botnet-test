331be9757f28dfc7214959b3386860a166c2c325
Simon Schagerer <schagerer.simon@inset.com>
2023-11-07 01:39:37 +0100

    Implement kalkulacka v5

--- Kalkulacka.cs
+++ Kalkulacka.cs
@@ -6,24 +6,99 @@ public static class Kalkulacka
 
         bool prevedeno1 = false;
         Console.WriteLine("Zadej cislo");
-        string? zadaneCislo = Console.ReadLine() ?? "";
+        string zadaneCislo1 = Console.ReadLine() ?? "";
         double prevedeneCislo1 = 0;
 
-        if (zadaneCislo.ToLower() == "x")
+        if (zadaneCislo1.ToLower() == "x")
         {
             return;
         }
-        prevedeno1 = double.TryParse(zadaneCislo, out prevedeneCislo1);
+        prevedeno1 = double.TryParse(zadaneCislo1, out prevedeneCislo1);
 
         while (prevedeno1 == false)
         {
             Console.WriteLine("Nezadal jsi platne cislo. Zadej spravne cislo.");
+            zadaneCislo1 = Console.ReadLine() ?? "";
+            if (zadaneCislo1.ToLower() == "x")
+            {
+                return;
+            }
+            prevedeno1 = double.TryParse(zadaneCislo1, out prevedeneCislo1);
+        }
+
+        double vysledek = prevedeneCislo1;
+        string operatorVstup;
+        bool prevedeno;
+        string zadaneCislo;
+        double prevedeneCislo;
+        while (true)
+        {
+            double nynejsiVstup = vysledek;
+            Console.WriteLine("Zadej operator");
+            operatorVstup = Console.ReadLine() ?? "";
+            if (operatorVstup.ToLower() == "x")
+            {
+                return;
+            }
+            
+            while (operatorVstup != "+" && operatorVstup != "-" && operatorVstup != "*" && operatorVstup != "/" && operatorVstup != "ˆ")
+            {
+                Console.WriteLine("Zadal jsi spatny operator, zadej spravny");
+                operatorVstup = Console.ReadLine() ?? "";
+                if (operatorVstup == "x")
+                {
+                    return;
+                }
+            }
+
+            Console.WriteLine("Zadej dalsi cislo");
             zadaneCislo = Console.ReadLine() ?? "";
             if (zadaneCislo.ToLower() == "x")
             {
                 return;
             }
-            prevedeno1 = double.TryParse(zadaneCislo, out prevedeneCislo1);
+            prevedeno = double.TryParse(zadaneCislo, out prevedeneCislo);
+
+            while (prevedeno == false)
+            {
+                Console.WriteLine("Nezdal jsi spravne cislo. Zadej znovu.");
+                zadaneCislo = Console.ReadLine() ?? "";
+                if (zadaneCislo.ToLower() == "x")
+                {
+                    return;
+                }
+                prevedeno = double.TryParse(zadaneCislo, out prevedeneCislo);
+            }
+
+            switch(operatorVstup)
+            {
+                case "*":
+                    vysledek = vysledek * prevedeneCislo;
+                    break;
+                case "/":
+                    if (prevedeneCislo == 0)
+                    {
+                        Console.WriteLine("Nelze delit nulout");
+                    }
+                    vysledek = vysledek / prevedeneCislo;
+                    break;
+                case "+":
+                    vysledek = vysledek + prevedeneCislo;
+                    break;
+                case "-":
+                    vysledek = vysledek - prevedeneCislo;
+                    break;
+                case "ˆ":
+                    for (int i = 0; i < (int)prevedeneCislo; i++)
+                    {
+                        vysledek = vysledek * prevedeneCislo;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Neznamy operand");
+                    break;
+            }
+            Console.WriteLine($"Vysledek: {nynejsiVstup} {operatorVstup} {prevedeneCislo} = {vysledek}");
         }
 
     }
